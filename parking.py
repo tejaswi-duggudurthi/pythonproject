0cb115186a81f0f018dc9f0abafb0f9e3316d450
tejaswi <tejaswiteju1812@gmail.com>
2022-12-05 12:49:33 +0530

    first commit

---
+++ parking.py
@@ -0,0 +1,17 @@
+# noOfBikes=0
+# noOfCars=0
+# totalPayment=0
+# noOfBikes=int(input("enter the no.bikes:"))
+# noOfCars=int(input("enter the no.cars:"))
+# totalPayment=noOfBikes*20+noOfCars*40
+# print("tatlpayment",totalPayment)
+qtyProduct1=0
+qtyProduct2=0
+qtyProduct3=0
+totalPayment=0
+qtyProduct1=int(input("enter the product1:"))
+qtyProduct2=int(input("enter the product2:"))
+qtyProduct3=int(input("enter the product3:"))
+totalPayment=qtyProduct1*40+qtyProduct2*50+qtyProduct3*60
+print("totalpayment=",totalPayment)
+
